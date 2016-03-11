48acf48c51511d5109243a0eeee5de54af16bdea
Cody Liu <liu@codylab.com>
2016-03-11 13:35:17 +0800

    Add Office 365 Login flow

---
+++ app/src/main/java/com/wiadvance/sipdemo/LoginActivity.java
@@ -0,0 +1,123 @@
+package com.wiadvance.sipdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.microsoft.aad.adal.AuthenticationCallback;
+import com.microsoft.aad.adal.AuthenticationCancelError;
+import com.microsoft.aad.adal.AuthenticationResult;
+import com.microsoft.aad.adal.UserInfo;
+import com.wiadvance.sipdemo.office365.AuthenticationManager;
+import com.wiadvance.sipdemo.office365.Constants;
+
+import java.net.URI;
+import java.util.UUID;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private Button mLoginButton;
+    private ProgressBar mLoginProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initializeViews();
+    }
+
+    private void initializeViews() {
+        mLoginButton = (Button) findViewById(R.id.login_button);
+        mLoginProgressBar = (ProgressBar) findViewById(R.id.login_progress_bar);
+    }
+
+    public void onLoginButtonClick(View view) {
+        Log.d(TAG, "onLoginButtonClick() called with: " + "view = [" + view + "]");
+
+        showConnectingInProgressUI();
+
+        checkO365Config();
+
+        final Intent sipIntent = new Intent(this, SipActivity.class);
+        AuthenticationManager.getInstance().setContextActivity(this);
+        AuthenticationManager.getInstance().connect(
+                new AuthenticationCallback<AuthenticationResult>() {
+                    @Override
+                    public void onSuccess(AuthenticationResult result) {
+
+                        //Need to get the new access token to the RESTHelper instance
+                        Log.i(TAG, "onConnectButtonClick onSuccess() - Successfully connected to Office 365");
+
+                        UserInfo info = result.getUserInfo();
+                        sipIntent.putExtra("givenName", info.getGivenName());
+                        sipIntent.putExtra("displayableId", info.getDisplayableId());
+                        startActivity(sipIntent);
+
+                        resetUI();
+                    }
+
+                    @Override
+                    public void onError(final Exception e) {
+                        Log.e(TAG, "onConnectButtonClick onError() - " + e.getMessage());
+                        if (!(e instanceof AuthenticationCancelError)) {
+                            showConnectErrorUI(e.getMessage());
+                        } else {
+                            resetUI();
+                        }
+                    }
+
+                });
+    }
+
+    private void showConnectingInProgressUI() {
+        mLoginButton.setVisibility(View.GONE);
+        mLoginProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void resetUI() {
+        mLoginButton.setVisibility(View.GONE);
+        mLoginProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void checkO365Config() {
+        //check that client id and redirect have been set correctly
+        try {
+            UUID.fromString(Constants.CLIENT_ID);
+            URI.create(Constants.REDIRECT_URI);
+        } catch (IllegalArgumentException e) {
+            Toast.makeText(
+                    this
+                    , getString(R.string.warning_clientid_redirecturi_incorrect)
+                    , Toast.LENGTH_LONG).show();
+
+            resetUI();
+            return;
+        }
+    }
+
+    private void showConnectErrorUI(String errorMessage) {
+        String msg = String.format(getResources().getString(
+                R.string.connect_toast_text_error), errorMessage
+        );
+
+        Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult() called with: " + "requestCode = [" + requestCode + "], resultCode = [" + resultCode + "], data = [" + data + "]");
+
+        AuthenticationManager
+                .getInstance()
+                .getAuthenticationContext()
+                .onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/com/wiadvance/sipdemo/office365/Constants.java
@@ -0,0 +1,10 @@
+package com.wiadvance.sipdemo.office365;
+
+public interface Constants {
+    String AUTHORITY_URL = "https://login.microsoftonline.com/common";
+    // Update these two constants with the values for your application:
+    String CLIENT_ID = "bc90ca19-2bf5-4f7d-abfd-9ae94f1241dc";
+    String REDIRECT_URI = "http://localhost/sip";
+    String MICROSOFT_GRAPH_API_ENDPOINT = "https://graph.microsoft.com/v1.0/";
+    String MICROSOFT_GRAPH_API_ENDPOINT_RESOURCE_ID = "https://graph.microsoft.com/";
+}
