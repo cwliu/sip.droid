e57d90d278ffccd005cdc907d7561a92b20d44cf
Cody Liu <liu@codylab.com>
2016-03-31 13:20:34 +0800

    Remove unused method

--- app/src/main/java/com/wiadvance/sipdemo/NotificationUtil.java
+++ app/src/main/java/com/wiadvance/sipdemo/NotificationUtil.java
@@ -1,6 +1,5 @@
 package com.wiadvance.sipdemo;
 
-import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
@@ -19,8 +18,6 @@ public class NotificationUtil {
     public static final String NOTIFY_CALL_STATUS = "notify_call_status";
     public static final String NOTIFY_CALL_IS_SIP = "is_sip_call";
 
-    public static final int INCOMING_CALL_NOTIFICATION_ID = 1;
-
     public static void displayStatus(Context context, String s) {
         Log.d(TAG, "displayStatus: " + s);
         Intent intent = new Intent(ACTION_NOTIFICATION);
@@ -34,11 +31,5 @@ public class NotificationUtil {
         intent.putExtra(NOTIFY_CALL_STATUS, message);
         intent.putExtra(NOTIFY_CALL_IS_SIP, isSip);
         LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
-    }
-
-    public static void cancelNotification(Context context) {
-        NotificationManager notificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.cancel(INCOMING_CALL_NOTIFICATION_ID);
     }
 }
