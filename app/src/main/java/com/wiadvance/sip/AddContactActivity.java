f78f13a437c1830c71933b45acd3fdaace617572
Cody Liu <liu@codylab.com>
2016-04-28 11:53:45 +0800

    Dynamic edittext input

--- app/src/main/java/com/wiadvance/sip/AddContactActivity.java
+++ app/src/main/java/com/wiadvance/sip/AddContactActivity.java
@@ -14,17 +14,16 @@ import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
-import com.wiadvance.sip.db.ContactTableHelper;
-import com.wiadvance.sip.model.Contact;
-
-import static android.widget.Toast.LENGTH_LONG;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddContactActivity extends AppCompatActivity {
 
     private static final String ARG_NAME = "ARG_NAME";
     private static final String ARG_PHONE = "ARG_PHONE";
+
+    private static final List<View> phoneEditTextList = new ArrayList<>();
 
     public static Intent newIntent(Context context) {
         return new Intent(context, AddContactActivity.class);
@@ -49,12 +48,12 @@ public class AddContactActivity extends AppCompatActivity {
     private void setupViewEvents() {
 
         final EditText nameEditText = (EditText) findViewById(R.id.add_contact_name_edittext);
-        final EditText phoneEditText = (EditText) findViewById(R.id.add_contact_phone_edittext);
+//        final EditText phoneEditText = (EditText) findViewById(R.id.add_contact_phone_edittext);
         final ImageButton addPhoneImageButton = (ImageButton) findViewById(R.id.add_contact_add_phone_image_button);
         final GridLayout contactGridLayout = (GridLayout) findViewById(R.id.add_contact_gridlayout);
-        final LinearLayout phonePoolLinearLayout = (LinearLayout) findViewById(R.id.phone_number_pool_linear_layout);
+        final LinearLayout phoneListLinearLayout = (LinearLayout) findViewById(R.id.phone_list_linear_layout);
 
-        if (contactGridLayout == null || phonePoolLinearLayout == null) {
+        if (contactGridLayout == null || phoneListLinearLayout == null) {
             return;
         }
 
@@ -64,44 +63,65 @@ public class AddContactActivity extends AppCompatActivity {
         if (nameEditText != null) {
             nameEditText.setText(name);
         }
-        String phone = getIntent().getStringExtra(ARG_PHONE);
-        if (phoneEditText != null) {
-            phoneEditText.setText(phone);
-        }
-
-        if (saveButton != null) {
-            saveButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    String name = nameEditText != null ? nameEditText.getText().toString() : "";
-                    String phone = phoneEditText != null ? phoneEditText.getText().toString() : "";
-
-                    if (name.equals("") || phone.equals("")) {
-                        Toast.makeText(getApplicationContext(), "請輸入名稱與電話", LENGTH_LONG).show();
-                        return;
-                    }
-
-                    Contact c = new Contact(name);
-                    c.setPhone(phone);
-                    c.setType(Contact.TYPE_PHONE_MANUAL);
-                    ContactTableHelper.getInstance(getApplicationContext()).addContact(c);
-
-                    Toast.makeText(getApplicationContext(), "已成功將 " + c.getName() + " 加到通訊錄", LENGTH_LONG).show();
-
-                    NotificationUtil.phoneContactUpdate(getApplicationContext());
-                    finish();
-                }
-            });
-        }
+//        String phone = getIntent().getStringExtra(ARG_PHONE);
+//        if (phoneEditText != null) {
+//            phoneEditText.setText(phone);
+//        }
+//
+//        if (saveButton != null) {
+//            saveButton.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//
+//                    String name = nameEditText != null ? nameEditText.getText().toString() : "";
+//                    String phone = phoneEditText != null ? phoneEditText.getText().toString() : "";
+//
+//                    if (name.equals("") || phone.equals("")) {
+//                        Toast.makeText(getApplicationContext(), "請輸入名稱與電話", LENGTH_LONG).show();
+//                        return;
+//                    }
+//
+//                    Contact c = new Contact(name);
+//                    c.setPhone(phone);
+//                    c.setType(Contact.TYPE_PHONE_MANUAL);
+//                    ContactTableHelper.getInstance(getApplicationContext()).addContact(c);
+//
+//                    Toast.makeText(getApplicationContext(), "已成功將 " + c.getName() + " 加到通訊錄", LENGTH_LONG).show();
+//
+//                    NotificationUtil.phoneContactUpdate(getApplicationContext());
+//                    finish();
+//                }
+//            });
+//        }
 
 
         //noinspection ConstantConditions
         addPhoneImageButton.setOnClickListener(new View.OnClickListener() {
+            public static final int VIEW_GROUP_KEY = 1;
+            public static final int EDIT_TEXT_KEY = 2;
+
             @Override
             public void onClick(View v) {
 
-                View phoneEditView = LayoutInflater.from(AddContactActivity.this).inflate(R.layout.list_item_phone_field, phonePoolLinearLayout, true);
+                View phoneViewGroup = LayoutInflater.from(AddContactActivity.this).inflate(R.layout.list_item_phone_field, phoneListLinearLayout, false);
+                phoneListLinearLayout.addView(phoneViewGroup);
+
+                EditText phoneEditText = (EditText) phoneViewGroup.findViewById(R.id.list_item_phone_field_edittext);
+                View removeButton = phoneViewGroup.findViewById(R.id.list_item_phone_field_remove);
+                removeButton.setTag(R.string.tag_viewgroup, phoneViewGroup);
+                removeButton.setTag(R.string.tag_edittext, phoneEditText);
+
+                phoneEditTextList.add(phoneEditText);
+                removeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        LinearLayout viewGroup = (LinearLayout) v.getTag(R.string.tag_viewgroup);
+                        EditText editText = (EditText) v.getTag(R.string.tag_edittext);
+
+                        phoneEditTextList.remove(editText);
+                        phoneListLinearLayout.removeView(viewGroup);
+                    }
+                });
             }
         });
     }
